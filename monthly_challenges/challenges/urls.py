8f0fdbad31d159919cb69950df68c54e996d26f8
Sri Vishnu Pasumarthi <srivishp@gmail.com>
2026-04-06 14:42:39 +0530

    Add reverse function and named URLs

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -6,11 +6,12 @@ urlpatterns = [
     # ? These are static paths
     # path("january", views.indexJan),
     # path("february", views.indexFeb)
+    path("", views.index),  # /challenges/
 
     # > This is a dynamic path
     # * <> holds an identifier within it, and the type of value it should be (str, int, etc.)
     path("<int:month>", views.monthly_challenge_by_number),
-
-    path("<str:month>", views.monthly_challenge)
+    # Creating a named URL
+    path("<str:month>", views.monthly_challenge, name="monthly_challenge")
 
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "January": "This works in January!",
@@ -28,22 +29,38 @@ monthly_challenges = {
 #     # Function takes a request and gives a response
 #     return HttpResponse("This works in February!")
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:        # Using reverse function to get the URL of the month
+        month_path = reverse("monthly_challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
+    response_data = f"<ul>{list_items}</ul >"
+    return HttpResponse(response_data)
 
 # > This is a dynamic view/captured value
 # * month is the identifier
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
         return HttpResponseNotFound("Invalid month!")
+
     redirect_month = months[month - 1]
+    # Reverse function takes the name of the URL and gives the actual URL back
+    redirect_path = reverse("monthly_challenge", args=[
+                            redirect_month])  # /challenge/january
     # f-string usage
-    return HttpResponseRedirect(f"/challenges/{redirect_month}")
-  #  return HttpResponse(f"This works for month number {month}!")
+    # return HttpResponseRedirect(f"/challenges/{redirect_month}")
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month.capitalize()]
-        return HttpResponse(challenge_text)
+        # Returning HTML instead of just text
+        response_data = f"<h1>{month.capitalize()}</h1><p>{challenge_text}</p>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
